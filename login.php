2329ffbc8de458b8e0c920adccd9ae9ef5d84418
Alex-Sprey <alexsprey1997@gmail.com>
2022-04-11 18:35:51 +0200

    V1.1

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/cssDB/style.css">
+  </head>
+  <body>
+
+    <h1>Login</h1>
+
+    <form action="login.php" method="POST">
+      <input name="email" type="text" placeholder="Enter your email">
+      <input name="password" type="password" placeholder="Enter your Password">
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
