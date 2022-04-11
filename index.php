9e0aa5429bf4093f0f25a775524c3a0025b3ed38
Alex-Sprey <alexsprey1997@gmail.com>
2022-04-11 18:30:41 +0200

    V1.0

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-prueba github
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bienvenido a la Ruleta de la Suerte</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/cssDB/style.css">
+  </head>
+  <body>
+      <h1>Please Login or SignUp</h1>
+
+      <a href="login.php">Login</a> or
+      <a href="signup.php">SignUp</a>
+  </body>
+</html>
