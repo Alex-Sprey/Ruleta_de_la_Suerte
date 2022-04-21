8e4726979db445c88d74b1a7dd4187a4aed78b9d
Alex-Sprey <alexsprey1997@gmail.com>
2022-04-21 16:40:10 +0200

    V1.5

--- juego/index2.php
+++ juego/index2.php
@@ -1,1 +1,21 @@
-aquí irá el panel de la ruleta
+<title>Ruleta de la suerte</title>
+
+	<div id="jugadores"></div>
+	<div class="form-group col-md-3">
+		<label class="form-label">Letra</label>
+		<input type="text" id="letra" placeholder="Introduzca letra">
+	</div>
+
+	<div class="button-group col-md-1">
+		<input type="button" id="letrar" value="Comprobar">
+	</div>
+			
+	<div class="form-group col-md-3">
+		<label class="form-label">Resolver</label>
+		<input type="text" id="respuesta" placeholder="Introduzca respuesta">
+	</div>
+	<div class="button-group col-md-1">
+		<input type="button" id="respuestar" value="Comprobar">
+	</div>
+	</div>
+
